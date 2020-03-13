8edcc9b49bc9fd581d2a994e81a3596b5ef9bb68
Stuart Harper <antisharper@gmail.com>
2020-03-13 11:08:43 -0400

    Fix scanit location

--- RouterSetup/doscan.sh
+++ RouterSetup/doscan.sh
@@ -6,7 +6,7 @@ TEMPSORTFILE=`mktemp`
 if [ -z "$SORT" ]; then
   SORT="1,5,7"
 fi
-  echo $SORT | sed 's/,/\n/g' | while read COLUMN; do 
+  echo $SORT | sed 's/,/\n/g' | while read COLUMN; do
     case $COLUMN in
       ES*|SID|SSID|1) ADDSTRING="-k1,1";;
       A*|2) ADDSTRING="-k2,2";;
@@ -20,9 +20,9 @@ fi
       default) ADDSTRING="";;
     esac
     SORTSTRING=`echo "$SORTSTRING $ADDSTRING" | tee $TEMPSORTFILE`
-  done  
+  done
 
 SORTSTRING=`cat $TEMPSORTFILE`
 rm -f $TEMPSORTFILE
 #echo EXCEUTE $SORTSTRING
-bash ~pi/scanit.sh  | sort -t, $SORTSTRING | awk -F,  '{printf ("%-22.22s %-17.17s %-9.9s %9.9s %7.7s %-7.7s %8.8s %7.7s %7.7s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9)}'
+bash /home/pi/scanit.sh | sort -t, $SORTSTRING | awk -F,  '{printf ("%-22.22s %-17.17s %-9.9s %9.9s %7.7s %-7.7s %8.8s %7.7s %7.7s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9)}'
