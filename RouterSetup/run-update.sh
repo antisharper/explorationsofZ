69f69533a1f36963acd7bfca625267f069c7682c
antisharper <61501728+antisharper@users.noreply.github.com>
2022-06-30 17:41:22 -0400

    Update run-update.sh

--- RouterSetup/run-update.sh
+++ RouterSetup/run-update.sh
@@ -20,9 +20,19 @@ shift $((OPTIND-1))
 
 if [ ! -z ${UPDATEACCOUNT} ]; then
   while (true); do
-          echo ---- "Trying Connectivity" --- `date`
-          sudo -u $RUNASUSER ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -N -R ${PORT}:localhost:22 ${UPDATEACCOUNT}
-          echo ---- "Connection failed ... Retrying in $WAITTIME seconds" --- `date`
+    echo ---- "Trying Connectivity" --- `date`
+    #sudo -u $RUNASUSER ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -N -R ${PORT}:localhost:22 ${UPDATEACCOUNT}
+    sudo -u $RUNASUSER \
+      ssh -g -C \
+	-R ${PORT}:localhost:22 \
+	-o ConnectTimeout=15 \
+	-o ExitOnForwardFailure=yes
+	-o BatchMode=yes \
+        -o ServerAliveCountMax=3 \
+        -o ServerAliveInterval=15
+        -o StrictHostKeyChecking=no
+	${UPDATEACCOUNT} sleep 1800
+      echo ---- "Connection failed ... Retrying in $WAITTIME seconds" --- PID:$$ --- `date`
     sleep $WAITTIME
   done
 else
