71c02d0956ff9e3e251f4967954684ce89888d91
antisharper <61501728+antisharper@users.noreply.github.com>
2022-10-30 08:37:28 -0400

    Update handle-logs.sh

--- handle-logs.sh
+++ handle-logs.sh
@@ -3,7 +3,7 @@
 ADATE=$(date +%Y%m%d-%H%M%S)
 FILES=$(ls -1 /dev/shm/run* /dev/shm/log-monitor.out)
 CONNECTIVITYHOST=connectivity@theharpers.homedns.org
-sleep 30
+
 cd /dev/shm
 mkdir TEMP
 cp $FILES /dev/shm/TEMP/
@@ -12,5 +12,6 @@ cd TEMP
 tar jcf ../log-monitor-$(hostname)-$ADATE.tb2 *
 cd ..
 chmod a+r log-monitor-$(hostname)-$ADATE.tb2
+chown pi:pi log-monitor-$(hostname)-$ADATE.tb2 
 rm -rf TEMP
-sudo -u pi scp log-monitor-$(hostname)-$ADATE.tb2 ${CONNECTIVITYHOST}:logs
+sudo -u pi rsync -r --remove-source-files --delete-after log-monitor-*.tb2 ${CONNECTIVITYHOST}:logs
